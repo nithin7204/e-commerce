c147a7d4ac7c2f5e1b5f828d64d40be0b8401871
nithin7204 <nithinkumarkunchala@gmail.com>
2025-03-08 15:58:30 +0530

    backend server changed to render

--- client/src/AddProduct/AddProduct.jsx
+++ client/src/AddProduct/AddProduct.jsx
@@ -13,7 +13,7 @@ export default function AddProduct() {
     }
     function addProduct(e) {
         e.preventDefault()
-        axios.post("http://localhost:5000/api/product/add", formData, {
+        axios.post("https://e-commerce-t5sy.onrender.com/api/product/add", formData, {
             headers: { Authorization: `Bearer ${user.token}` }
         })
             .then((res) => {
